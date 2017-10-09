2f466fddafb4dafa5970692d309cb12c3cd0028c
TPeterW <peterwangtao0@hotmail.com>
2017-10-09 18:06:45 -0400

    Feature extraction

--- feature_extraction/extract_features.py
+++ feature_extraction/extract_features.py
@@ -2,8 +2,9 @@
 # -*- coding: utf-8 -*-
 
 import sys
-
 import pandas as pd
+
+from tsfresh import extract_features
 
 def main():
 	if len(sys.argv) < 2:
@@ -15,13 +16,15 @@ def main():
 	raw_price_data = pd.DataFrame.from_csv(filename, index_col=None, header=0)
 	raw_price_data = raw_price_data[raw_price_data.columns[1:]]
 	
-	features = extract(raw_price_data)
+	timeseries = convert(raw_price_data)
+	features = extract_features(timeseries, column_id='id', column_sort='time')
+	features.to_csv('features_extracted.csv', sep=',', index=False, header=True)
 
-	print(features)
 
-def extract(raw_price_data):
+def convert(raw_price_data):
 	percent_price_data = pd.DataFrame(index=range(len(raw_price_data)), columns=raw_price_data.columns, dtype=float)
 
+	# convert to fluctuation percentage
 	prev = raw_price_data.loc[0]
 	for index, row in raw_price_data.iterrows():
 		percent_price_data.loc[index]['Open'] = row['Open'] / prev['Open']
@@ -31,7 +34,15 @@ def extract(raw_price_data):
 
 		prev = raw_price_data.loc[index]
 
-	return percent_price_data
+	raw = []
+	for i in range(30, len(percent_price_data)):
+		for j in range(30):
+			row = percent_price_data.loc[i - 30 + j].tolist()
+			raw.append([i - 30, j, row[0], row[1], row[2], row[3]])
+
+	timeseries = pd.DataFrame(raw, index=None, columns=['id', 'time', 'Open', 'High', 'Low', 'Close'])
+
+	return timeseries
 
 if __name__ == '__main__':
 	main()
