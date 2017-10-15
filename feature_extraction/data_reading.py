863df97c578fe08dfc7ed1f5c1e9fb4085c7d5c6
Henry Swaffield <hhswaff@gmail.com>
2017-10-15 15:12:41 -0400

    Cleaned data manually, reading in from multiple files, organizing

--- feature_extraction/data_reading.py
+++ feature_extraction/data_reading.py
@@ -4,6 +4,12 @@
 import sys
 import pandas as pd
 
+def main():
+    filepath = sys.argv[1]
+    filenames = [filepath + "btc-ohlc-coindesk.csv", filepath + "n-daily-btc-transactions.csv", filepath + "num-btc-transactions-per-block.csv", filepath + "btc-estimated-transaction-volume-blockchain-in-usd.csv"]
+    filenames.append(filepath + "fee-cost-per-transaction-percent.csv")
+    read_files(filenames)
+
 def read_files(data_files):
     print('hi')
 
@@ -12,6 +18,15 @@ def read_files(data_files):
 
     for filename in data_files:
         print(filename)
-        file_data = pd.read_csv(filename, index_col=None, header=0, squeeze=True, usecols=[1], skiprows=[0])
+        data_name = filename.split('/')[-1].split('.')[0]
+        # set the data col to the filename... cleaner...
+        # TODO: Look out for data where there is randomly new data frequencies... sometimes it changes...
+        file_data = pd.read_csv(filename, index_col=None, header=0, nrows=1370)
         data[filename] = file_data
+        print(file_data)
+        # print("data rows:")
+        # print(file_data[data_name])
     return data
+
+if __name__ == '__main__':
+	main()
