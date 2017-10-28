fb12075ded9e098651ff9046fe7936dd14f58d3d
TPeterW <peterwangtao0@hotmail.com>
2017-10-28 15:34:07 -0400

    Features extraction for different number of features

--- feature_extraction/extract_features.py
+++ feature_extraction/extract_features.py
@@ -14,10 +14,11 @@ def main():
 		exit(1)
 	filename = sys.argv[1]
 
-	raw_price_data = pd.DataFrame.from_csv(filename, index_col=None, header=0)
-	raw_price_data = raw_price_data[raw_price_data.columns[1:]]
-	
+	raw_price_data = pd.read_csv(filename, index_col=None, header=0, thousands=',')
+	raw_price_data = raw_price_data[raw_price_data.columns[1:]]		# get rid of the date columns
+
 	timeseries, labels = convert(raw_price_data)
+
 	features = extract_features(timeseries, column_id='id', column_sort='time')
 	impute(features)
 	features.reset_index(drop=True, inplace=True)
@@ -25,33 +26,37 @@ def main():
 	features.to_csv('features_extracted.csv', sep=',', index=False, header=True)
 	labels.to_csv('labels.csv', sep=',', index=False, header=False)
 
+	print(features)
+
 def convert(raw_price_data, percentage=False):
-	percent_price_data = pd.DataFrame(index=range(len(raw_price_data)), columns=raw_price_data.columns, dtype=float)
+	price_data = raw_price_data.astype(float)
 
-	if percentage:
-		# convert to fluctuation percentage
-		prev = raw_price_data.loc[0]
-		for index, row in raw_price_data.iterrows():
-			percent_price_data.loc[index]['Open'] = row['Open'] / prev['Open']
-			percent_price_data.loc[index]['High'] = row['High'] / prev['High']
-			percent_price_data.loc[index]['Low'] = row['Low'] / prev['Low']
-			percent_price_data.loc[index]['Close'] = row['Close'] / prev['Close']
-
-			prev = raw_price_data.loc[index]
-		price_data = raw_price_data
-	else:
-		labels = raw_price_data['Close'][30:]
-		price_data = raw_price_data
+	labels = price_data['btc-ohlc-coindesk-Close'][30:]
 
 	raw = []
-	for i in range(30, len(percent_price_data)):
+	for i in range(30, len(price_data)):
 		for j in range(30):
 			row = price_data.loc[i - 30 + j].tolist()
-			raw.append([i - 30, j, row[0], row[1], row[2], row[3]])
+			raw.append([i - 30, j] + row)
 
-	timeseries = pd.DataFrame(raw, index=None, columns=['id', 'time', 'Open', 'High', 'Low', 'Close'])
+	timeseries = pd.DataFrame(raw, index=None, columns=['id', 'time']+price_data.columns.tolist())
 
 	return timeseries, labels
 
 if __name__ == '__main__':
 	main()
+
+# if percentage:
+	# 	# convert to fluctuation percentage
+	# 	prev = raw_price_data.loc[0]
+	# 	for index, row in raw_price_data.iterrows():
+	# 		percent_price_data.loc[index]['Open'] = row['Open'] / prev['Open']
+	# 		percent_price_data.loc[index]['High'] = row['High'] / prev['High']
+	# 		percent_price_data.loc[index]['Low'] = row['Low'] / prev['Low']
+	# 		percent_price_data.loc[index]['Close'] = row['Close'] / prev['Close']
+
+	# 		prev = raw_price_data.loc[index]
+	# 	price_data = raw_price_data
+	# else:
+	# 	labels = raw_price_data['Close'][30:]
+	# 	price_data = raw_price_data
--- prediction/test_logistic.py
+++ prediction/test_logistic.py
@@ -2,6 +2,7 @@
 # -*- coding: utf-8 -*-
 
 import sys
+import locale
 import itertools
 import numpy as np
 import pandas as pd
