850cc36d26e2621fed59884ed093fad379238791
Henry Swaffield <hhswaff@gmail.com>
2017-11-16 18:05:51 -0500

    more experiments, no substantial improvements

--- prediction/practice_lstm.py
+++ prediction/practice_lstm.py
@@ -19,6 +19,9 @@ def main():
     # size = 200000
     split = int(size * .6)
 
+    # Coinbase data set is sparse before row 418053... there are no changes, so it is useles...
+    useless_rows = 418053
+
     # When the split is .4 max accuracy is 52% on test set
     # when split is .6 max accuracy is 59% on test set
     # when split is .8 max accuracy is 62.83% on test set
@@ -39,7 +42,7 @@ def main():
 
     filename = "/Users/HenrySwaffield/Documents/Middlebury Senior Year/fall/Senior Seminar/project/data/raw_data/custom_minute_test1.csv"
 
-    file_data = pd.read_csv(filename, index_col=None, header=0, nrows=size)
+    file_data = pd.read_csv(filename, skiprows=useless_rows, index_col=None, header=0, nrows=size)
     file_data = file_data[file_data.columns[1:]]
 
     matrix = pd.DataFrame.as_matrix(file_data)
@@ -66,7 +69,46 @@ def main():
 
     non_labels = matrix[:, 1:7]
 
-    y_train = matrix[:, 7:]
+    # One minute later guesses, as labels:
+    y_train = matrix[:, 7:8]
+    # Getting about 61% accuracy... not that much better than before...
+    # 62910 / 62910[ == == == == == == == == == == == == == == ==] - 301
+    # s - loss: 0.6583 - acc: 0.6119 - val_loss: 0.6639 - val_acc: 0.5937
+    # Epoch
+    # 2 / 20
+    # 62910 / 62910[ == == == == == == == == == == == == == == ==] - 312
+    # s - loss: 0.6550 - acc: 0.6122 - val_loss: 0.6630 - val_acc: 0.5961
+
+
+    # represents the 10 minute later guesses...
+    # Gets around 55% accuracy...
+    # y_train = matrix[:, 8:]
+
+    # 6291 / 6291[ == == == == == == == == == == == == == == ==] - 28
+    # s - loss: 0.6885 - acc: 0.5498 - val_loss: 0.6898 - val_acc: 0.5510
+    # Epoch
+    # 2 / 20
+    # 6291 / 6291[ == == == == == == == == == == == == == == ==] - 28
+    # s - loss: 0.6801 - acc: 0.5680 - val_loss: 0.6973 - val_acc: 0.5033
+    # Epoch
+    # 3 / 20
+    # 6291 / 6291[ == == == == == == == == == == == == == == ==] - 28
+    # s - loss: 0.6764 - acc: 0.5811 - val_loss: 0.6936 - val_acc: 0.5346
+    # Epoch
+    # 4 / 20
+    # 6291 / 6291[ == == == == == == == == == == == == == == ==] - 29
+    # s - loss: 0.6741 - acc: 0.5800 - val_loss: 0.7104 - val_acc: 0.4700
+    # Epoch
+    # 5 / 20
+    # 6291 / 6291[ == == == == == == == == == == == == == == ==] - 30
+    # s - loss: 0.6708 - acc: 0.5912 - val_loss: 0.7034 - val_acc: 0.4731
+    # Epoch
+    # 6 / 20
+    # Overfitting seems to be an issue...
+
+    # average over the next 20 minutes being higher than current close...:
+    # this does not work well, with smaller data set...
+    # y_train = matrix[:, 9:10]
 
     print('y_train', y_train)
 
@@ -118,7 +160,8 @@ def main():
     x = non_labels[split:size-offset]
     y = y_train[split:]
 
-    model.fit(x_train_set, y_train_set, batch_size=5, epochs=20, validation_data=(x,y))
+    # bigger batch sizes makes traning much faster.
+    model.fit(x_train_set, y_train_set, batch_size=5, epochs=5, validation_data=(x,y))
     # give the right test set here... labels might be off...
     score = model.evaluate(non_labels[split:size-offset], y_train[split:], batch_size=2)
     print(score)
