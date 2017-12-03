9131e879a876e3b24e632340cb5aef0370334a55
Henry Swaffield <hhswaff@gmail.com>
2017-12-03 13:00:39 -0500

    moved more control higher up, pipeline now runs the labeling and feature extraction processes

--- automated_predictions.py
+++ automated_predictions.py
@@ -17,7 +17,7 @@ from sklearn.linear_model import LinearRegression, LogisticRegression, Lasso
 from sklearn.neighbors import KNeighborsClassifier
 from sklearn.metrics import accuracy_score, precision_score, recall_score, confusion_matrix
 
-from automated_simulation_pipeline import input_file_to_output_name
+import automated_simulation_pipeline
 
 
 def main():
@@ -31,47 +31,69 @@ def main():
     raw_data_name = sys.argv[3]
     minutes_before = int(sys.argv[4])
 
-    predict(features, labels, 0.9, minutes_before, raw_data_name)
+    # predict(features, labels, 0.9, minutes_before, raw_data_name)
 
 
 # Split this into train and predict?
-def predict(features, labels, cutoff, minutes_before, raw_data_name):
-    cutoff = int(len(features) * cutoff)
+def train(features_train, labels_train):
+    # cutoff = int(len(features) * cutoff)
 
-    labels = labels[:len(features)].astype(int)
-    features = select_features(features, labels)
+    # labels = labels[:len(features)].astype(int)
+    # features = select_features(features, labels)
 
     # TODO: move this outside of this file, to the pipeline level:
-    features_train = features.loc[:cutoff]
-    labels_train = labels.loc[:cutoff]
-    features_test = features.loc[cutoff:].reset_index(drop=True)
-    labels_test = labels.loc[cutoff:].reset_index(drop=True)
+    # features_train = features.loc[:cutoff]
+    # labels_train = labels.loc[:cutoff]
+    # features_test = features.loc[cutoff:].reset_index(drop=True)
+    # labels_test = labels.loc[cutoff:].reset_index(drop=True)
 
 
 
-    lr = LogisticRegression(C=1e5)
-    lr.fit(features_train, labels_train)
+    model = LogisticRegression(C=1e5)
+    model.fit(features_train, labels_train)
 
-    predictions = lr.predict(features_test)
-    score = lr.score(features_test, labels_test)
+    # predictions = model.predict(features_test)
+    # score = model.score(features_test, labels_test)
 
+    return model
 
-    # This will be in the pipeline
-    raw_data_offset = minutes_before + cutoff
-
-
-
-    np.savetxt(input_file_to_output_name(raw_data_name) + '_predictions.csv', predictions, fmt='%i', delimiter=',')
-
-    print('Accuracy: %s' % accuracy_score(labels_test, predictions))
-    print('Precision: %s' % precision_score(labels_test, predictions))
-    print('Recall: %s' % recall_score(labels_test, predictions))
+    #
+    #
+    #
+    # np.savetxt(input_file_to_output_name(raw_data_name) + '_predictions.csv', predictions, fmt='%i', delimiter=',')
+    #
+    # print('Accuracy: %s' % accuracy_score(labels_test, predictions))
+    # print('Precision: %s' % precision_score(labels_test, predictions))
+    # print('Recall: %s' % recall_score(labels_test, predictions))
 
     # Does not make sense to show this in this part of the code:
     # cnf_matrix = confusion_matrix(labels_test, predictions, [0, 1])
     # plt.figure()
     # plot_confusion_matrix(cnf_matrix, normalize=True, classes=[0, 1], title='Nomalized Confusion Matrix')
     # plt.show()
+
+def test_model(trained_model, features_test, labels_test, descriptive_output_name):
+    predictions = trained_model.predict(features_test)
+    score = trained_model.score(features_test, labels_test)
+
+    np.savetxt(descriptive_output_name + '_predictions.csv', predictions, fmt='%i', delimiter=',')
+
+    print('Accuracy: %s' % accuracy_score(labels_test, predictions))
+    print('Precision: %s' % precision_score(labels_test, predictions))
+    print('Recall: %s' % recall_score(labels_test, predictions))
+    print('Score: %s' % score)
+
+    # Does not make sense to show this in this part of the code:
+    # cnf_matrix = confusion_matrix(labels_test, predictions, [0, 1])
+    # plt.figure()
+    # plot_confusion_matrix(cnf_matrix, normalize=True, classes=[0, 1], title='Nomalized Confusion Matrix')
+    # plt.show()
+
+
+def train_and_test_process(features_train, labels_train, features_test, labels_test, descriptive_output_name):
+    model = train(features_train, labels_train)
+
+    test_model(model, features_test, labels_test, descriptive_output_name)
 
 
 def plot_confusion_matrix(cm, classes,
--- automated_simulation_pipeline.py
+++ automated_simulation_pipeline.py
@@ -1,5 +1,9 @@
-
+import sys
 import os.path
+from tsfresh import select_features
+
+import automated_feature_extraction
+import automated_predictions
 
 # Steps:
 
@@ -7,6 +11,8 @@ import os.path
 
 steps_look_back = 30
 raw_data = '/Users/HenrySwaffield/Documents/Middlebury Senior Year/fall/Senior Seminar/project/workspace/Bitcoin-Price-Prediction/prediction/input_test1.csv'
+
+global train_test_ratio
 
 # Auto Extraction Script: In: raw data, minutes_look_back. Out: Extracted Features, Labels (perhaps to change)
 
@@ -26,8 +32,47 @@ raw_data = '/Users/HenrySwaffield/Documents/Middlebury Senior Year/fall/Senior S
 # manage descriptive name here...
 
 
+# This takes output from the feature extraction and labeling components:
+# Reads csv... returns more convenient data_frames
+def split_train_and_test(features, labels):
+    global train_test_ratio
+
+    split_point = int(len(features) * train_test_ratio)
+
+    labels = labels[:len(features)].astype(int)
+    features = select_features(features, labels)
+
+    features_train = features.loc[:split_point]
+    labels_train = labels.loc[:split_point]
+    features_test = features.loc[split_point:].reset_index(drop=True)
+    labels_test = labels.loc[split_point:].reset_index(drop=True)
+
+    return features_train, labels_train, features_test, labels_test
+
+
 def input_file_to_output_name(filename):
     get_base_file = os.path.basename(filename)
     base_filename = get_base_file.split('.')[0]
     # base_filename = '/pipeline_data/' + base_filename
     return base_filename
+
+
+# maybe more params to specify pipeline options:
+# TODO: make a seperate pipeline function
+def main():
+    global train_test_ratio
+
+    raw_data_name = sys.argv[1]
+
+    train_test_ratio = float(sys.argv[2])
+
+    steps_look_back = int(sys.argv[3])
+
+    # Can check here to see if this stage is necessary:
+    automated_feature_extraction.feature_extraction_process(raw_data_name, steps_look_back)
+
+
+
+
+if __name__ == '__main__':
+    main()
