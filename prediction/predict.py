d27aaaa0c1177807219899f1c1e43f24c330c2b3
Henry Swaffield <hhswaff@gmail.com>
2017-12-03 13:54:12 -0500

    Cleaning up notes / small changes from original prediction script

--- prediction/predict.py
+++ prediction/predict.py
@@ -7,10 +7,7 @@ import itertools
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
-import csv
-
 from tsfresh import select_features
-
 from sklearn.svm import SVC
 from sklearn.tree import DecisionTreeClassifier
 from sklearn.linear_model import LinearRegression, LogisticRegression, Lasso
@@ -18,29 +15,18 @@ from sklearn.neighbors import KNeighborsClassifier
 from sklearn.metrics import accuracy_score, precision_score, recall_score, confusion_matrix
 
 
-# Will make a new script that is organized differently, to accomplish similar goals, within the context of the pipeline.
-
 def main():
 	if len(sys.argv) < 3:
 		print('Usage: ./predict.py features labels')
 		exit(1)
-	
+
 	features = pd.read_csv(sys.argv[1], index_col=None, header=0)
 	labels = pd.read_csv(sys.argv[2], index_col=None, header=None, squeeze=True)
 
-	# to organize:
-	# these should be synchronized with extract_features.py
-	raw_data = pd.read_csv(sys.argv[3], index_col=None, header=0, squeeze=True)
-	# minutes before:
-	minutes_before = int(sys.argv[4])
+	predict(features, labels, 0.9)
 
-	predict(features, labels, raw_data, 0.9, minutes_before)
 
-# TODO: also take a raw data parameter, do this in one step with the feature extraction??
-# goal with that is to have direct access to the price data... could simply pass as a param...
-# This could be handled by one super process, so that it is all known...
-# This will occur in the new file. This here is essentially an outline of the new pipeline component. Will remove comments soon.
-def predict(features, labels, raw_data, cutoff, minutes_before):
+def predict(features, labels, cutoff):
 	cutoff = int(len(features) * cutoff)
 
 	labels = labels[:len(features)].astype(int)
@@ -66,31 +52,8 @@ def predict(features, labels, raw_data, cutoff, minutes_before):
 	# for i, val in enumerate(predictions):
 	# 	predictions[i] = 1 if val > actual_prices[i] else 0
 
-	# TODO: get the original dataname to write a file, or atleast pass that to the simulation script
-	# Perhaps rework how this csv writing occurs, seperate csvs??
-
-
-	# rows - minutes_before from cutoff onwards shows the correct data...
-	# raw_data = raw_data[minutes_before + cutoff:]
-
-	raw_data_offset = minutes_before + cutoff
-
-
-	num_rows = len(predictions)
-	num_fields = 2
-	fieldnames = ['predicted', 'true', 'change']
-	with open("predictions.csv", 'w') as csvfile:
-		# w is write only, and it will overwrite an existing file (truncates to 0 length)
-		writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
-
-		writer.writeheader()
-
-		for i in range(num_rows):
-			current_row = {}
-			current_row[fieldnames[0]] = predictions[i]
-			current_row[fieldnames[1]] = labels_test[i]
-			current_row[fieldnames[2]] = raw_data['Change'][i + raw_data_offset]
-			writer.writerow(current_row)
+	# Writing the predictions to an output file:
+	np.savetxt('predictions.csv', predictions, fmt='%i', delimiter=',')
 
 	print('Accuracy: %s' % accuracy_score(labels_test, predictions))
 	print('Precision: %s' % precision_score(labels_test, predictions))
