f6995fe66e344f28fe660310d573056bb045d9b7
Henry Swaffield <hhswaff@gmail.com>
2017-12-06 19:11:46 -0500

    LSTM taking in extracted features, in pipeline

--- automated_lstm_predictions.py
+++ automated_lstm_predictions.py
@@ -6,13 +6,49 @@ import matplotlib.pyplot as plt
 import pandas as pd
 import math
 import matplotlib.pyplot as plt
-from keras.models import Sequential
-from keras.layers import Dense, Dropout
-from keras.layers import Embedding
-from keras.layers import LSTM
+# from keras.models import Sequential
+# from keras.layers import Dense, Dropout
+# from keras.layers import Embedding
+# from keras.layers import LSTM
 
 # TODO: follow the guide....
 # helpful reference: https://machinelearningmastery.com/time-series-prediction-lstm-recurrent-neural-networks-python-keras/
+
+
+# Takes in Dataframes:
+def pipeline_lstm(train_frame, labels_frame, cv_train_frame, cv_labels_frame):
+    # Importing these within this context, so that they are not loaded every time the pipeline is run:
+    from keras.models import Sequential
+    from keras.layers import Dense, Dropout
+    from keras.layers import Embedding
+    from keras.layers import LSTM
+
+    # print(train_frame)
+    # print(labels_frame)
+
+    train = train_frame.as_matrix()
+    labels = labels_frame.as_matrix()
+
+    print(train.shape)
+    print(labels.shape)
+
+    # Don't expect there to be timesteps... because features were extracted into timeseries, seperately...
+    # Could explore how to build timesteps into this here...
+
+    samples, dimension = train.shape
+    train = train.reshape((samples, 1, dimension))
+
+    model = Sequential()
+    # input_shape of LSTM first param is time steps...?
+    model.add(LSTM(128, input_shape=train.shape[1:]))
+    model.add(Dense(10))
+    model.add(Dense(1, activation='sigmoid'))
+    model.compile(loss='binary_crossentropy',
+                  optimizer='rmsprop',
+                  metrics=['accuracy'])
+
+    model.fit(train, labels)
+
 
 def main():
 
@@ -21,19 +57,12 @@ def main():
     # size = 200000
     split = int(size * .6)
 
-    # Coinbase data set is sparse before row 418053... there are no changes, so it is useles...
-    useless_rows = 418053
-
+    # to be a param...
     filename = "/Users/HenrySwaffield/Documents/Middlebury Senior Year/fall/Senior Seminar/project/data/raw_data/custom_minute_test1.csv"
 
-    file_data = pd.read_csv(filename, skiprows=useless_rows, index_col=None, header=0, nrows=size)
-    file_data = file_data[file_data.columns[1:]]
+    file_data = pd.read_csv(filename, index_col=None, header=0, nrows=size)
 
     matrix = pd.DataFrame.as_matrix(file_data)
-
-    x_train = matrix[:, 1:12]
-
-
 
 
     # Setting up model:
@@ -57,23 +86,6 @@ def main():
                   metrics=['accuracy'])
 
 
-    # x_train = matrix[:, 1:4]
-    # y_train = matrix[:, 4]
-    # np.delete(x_train, size, 0)
-    # np.delete(x_train, 0, 0)
-
-    # print(x_train)
-    # print(y_train)
-
-    # print(file_data)
-
-
-    # trying offsets...
-
-    x_train_set = non_labels[:split]
-    y_train_set = y_train[0:split]
-    print(x_train_set)
-    print(y_train_set)
 
     x = non_labels[split:size-offset]
     y = y_train[split:]
