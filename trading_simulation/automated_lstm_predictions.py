e120b785518b927dd6c3fb00bea79a1578a82816
Henry Swaffield <hhswaff@gmail.com>
2017-12-07 16:31:45 -0500

    Commandline Runnable LSTM

--- trading_simulation/automated_lstm_predictions.py
+++ trading_simulation/automated_lstm_predictions.py
@@ -1,28 +1,17 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
-import numpy as np
-import sys
-import matplotlib.pyplot as plt
 import pandas as pd
-import math
-import matplotlib.pyplot as plt
-# from keras.models import Sequential
-# from keras.layers import Dense, Dropout
-# from keras.layers import Embedding
-# from keras.layers import LSTM
-
-# TODO: follow the guide....
-# helpful reference: https://machinelearningmastery.com/time-series-prediction-lstm-recurrent-neural-networks-python-keras/
-
+import numpy as np
+from tsfresh import select_features
+import sys
 
 # Takes in Dataframes:
 def pipeline_lstm(train_frame, labels_frame, cv_features_frame, cv_labels_frame):
     # Importing these within this context, so that they are not loaded every time the pipeline is run:
     from keras.models import Sequential
     from keras.layers import Dense, Dropout
-    from keras.layers import Embedding
-    from keras.layers import LSTM, ConvLSTM2D
+    from keras.layers import LSTM
 
     # print(train_frame)
     # print(labels_frame)
@@ -52,12 +41,37 @@ def pipeline_lstm(train_frame, labels_frame, cv_features_frame, cv_labels_frame)
                   optimizer='rmsprop',
                   metrics=['accuracy'])
 
-    model.fit(train, labels, batch_size=10, epochs=10, validation_data=(cv_features,cv_labels))
+    model.fit(train, labels, batch_size=10, epochs=100, validation_data=(cv_features,cv_labels))
     print('LSTM Training Complete')
     score = model.evaluate(cv_features,cv_labels)
 
     print(score)
 
+
+# Should import this from automated_simulation_pipeline:
+# reads the output from the labeling and feature extraction processes:
+def read_feature_extraction_and_label_output(features, labels):
+    features = pd.read_csv(features, index_col=None, header=0)
+    labels = pd.read_csv(labels, index_col=None, header=None, squeeze=True)
+
+    return features, labels
+
+# This takes output from the feature extraction and labeling components:
+# Reads csv... returns more convenient data_frames
+def split_train_and_test(features, labels):
+    train_test_ratio = 0.6
+
+    split_point = int(len(features) * train_test_ratio)
+
+    labels = labels[:len(features)].astype(int)
+    features = select_features(features, labels)
+
+    features_train = features.loc[:split_point]
+    labels_train = labels.loc[:split_point]
+    features_test = features.loc[split_point:].reset_index(drop=True)
+    labels_test = labels.loc[split_point:].reset_index(drop=True)
+
+    return features_train, labels_train, features_test, labels_test
 
 # A commandline runnable version:
 def main():
@@ -65,8 +79,11 @@ def main():
         print('Usage: ./automated_lstm_predictions.py extracted_features labels')
         exit(1)
 
-    #Simply Would Need To Parse And Call The Above Process
 
+    # Parse the output of upstream processes:
+    features, labels = read_feature_extraction_and_label_output(str(sys.argv[1]), str(sys.argv[2]))
+    features_train, labels_train, features_test, labels_test = split_train_and_test(features, labels)
+    pipeline_lstm(features_train, labels_train, features_test, labels_test)
 
 
 if __name__ == '__main__':
